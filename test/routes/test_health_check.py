5f5a5cea69a08dde6e8148b03a74f1e220d6b24a
Orion Montoya Correa <orionmontoyac@gmail.com>
2023-03-06 12:46:22 -0500

    Health check endpoint unit tests  added.

---
+++ test/routes/test_health_check.py
@@ -0,0 +1,25 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    with create_app().test_client() as client:
+        yield client
+
+
+class TestHealth:
+
+    def setup_method(self):
+        self.health_check_url = "/api/v1/health"
+
+    def test_get_health_check_ok(self, client):
+        response = client.get(self.health_check_url)
+
+        assert response.status_code == 200
